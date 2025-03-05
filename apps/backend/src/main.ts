4a75b6c42f5a4f18da9b4214743d94585711dcb6
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-03-05 20:10:06 +0200

    modifying swagger auth method

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,17 +21,22 @@ async function bootstrap() {
       },
     })
   );
+  app.use(cookieParser());
 
   const allowedOrigins = (
     configService.get<string>('ALLOWED_ORIGINS', 'http://localhost:4200')
       .split(',')
-      .map(origin => origin.trim())
+      .map(origin =>{
+        origin = origin.trim();
+        const url = new URL(origin);
+        return `${url.protocol}//${url.host}`;
+      })
   );
 
-
+  logger.log(`those are the allowed origins: ${JSON.stringify(allowedOrigins)}`);
   app.enableCors({
     origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         logger.warn(`Blocked CORS request from: ${origin}`);
@@ -51,13 +58,27 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
   });
 
-  const config = new DocumentBuilder()
+   const config = new DocumentBuilder()
     .setTitle('Auth API')
-    .setDescription('User Authentication API')
+    .setDescription(
+      'User Authentication API. After signing in, the access token is set as an HTTP‑only cookie named "access_token". This cookie is automatically sent with requests. To test protected endpoints, first use the /auth/signin endpoint to set the cookie.'
+    )
     .setVersion('1.0')
-    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
+  document.components = {
+    ...document.components,
+    securitySchemes: {
+      cookieAuth: {
+        type: 'apiKey',
+        in: 'cookie',
+        name: 'access_token',
+      },
+    },
+  };
+
+
   SwaggerModule.setup('api-docs', app, document);
 
   const port = configService.get<number>('PORT', 3000);
