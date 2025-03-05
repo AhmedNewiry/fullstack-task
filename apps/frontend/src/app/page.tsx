33fb4d586425ad0288403040d078de1b8bba24ec
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-03-05 20:17:20 +0200

    updating app home page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,17 +1,7 @@
-'use client';
-
-import Navbar from './components/NavBar';
+import HomePageContent from "./HomePageContent";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-
-      <main className="flex-grow flex items-center justify-center">
-        <h2 className="text-3xl font-bold text-gray-800">
-          Welcome to the application.
-        </h2>
-      </main>
-
-    </div>
+   <HomePageContent/>
   );
 }
