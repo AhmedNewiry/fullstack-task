5699898cd5c5adbc481f25767cdc8946b1836754
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-03-05 20:19:52 +0200

    updating sinup on sumbint method and button styles

--- apps/frontend/src/app/sign-up/MainContent.tsx
+++ apps/frontend/src/app/sign-up/MainContent.tsx
@@ -6,21 +6,43 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
 import { signupSchema, SignupFormInputs } from '../../schemas/sign-up.schema';
 import FormInput from '../components/FormInput';
+import { useState } from 'react';
 export default function Signup() {
   const router = useRouter();
   const { register, handleSubmit, formState: { errors } } = useForm<SignupFormInputs>({
     resolver: zodResolver(signupSchema)
   });
+  const [errorMessage, setErrorMessage] = useState('');
 
   const onSubmit = async (data: SignupFormInputs) => {
+
+    const { email, name, password } = data;
+    try {
  
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/signup`,
+        { email, name, password }
+      );
+      
+      if (response.status === 201 || response.status === 200) {
+   
+        router.push('/sign-in');
+      }
+    } catch (error: any) {
+
+      if (error.response?.data?.message) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage('An error occurred. Please try again.');
+      }
+    }
   };
 
   return (
-    <div className="flex h-[calc(100vh-96px)] items-center justify-center bg-gray-100">
+    <div className="flex h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6 space-y-6">
         <h1 className="text-2xl font-bold text-center text-gray-800">Sign Up</h1>
-        
+        {errorMessage && <p className="text-red-500 text-center">{errorMessage}</p>}
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
           <FormInput
             label="Email"
@@ -55,7 +77,7 @@ export default function Signup() {
 
           <button
             type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
+            className="w-full bg-[#500878] hover:bg-[#420663] text-white font-bold py-4 px-8 rounded-full transition duration-300"
           >
             Sign Up
           </button>
